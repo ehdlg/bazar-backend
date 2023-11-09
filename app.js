ce28fca1fd820474227ef10e0d82e9d55fc072f5
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-11-09 09:09:54 +0100

    Middleware for handling 404 requests

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use('/api', apiRoutes);
 
+app.use((req, res) => {
+  return res.status(404).json({ error: 'Not found' });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening on http://localhost:${process.env.PORT}`);
 });
